d2a62aae45a8b45679da02debe7c8ad7f226969f
nurulauliyasisfo <nurulauliyaamin26@gmail.com>
2025-11-09 20:25:46 +0800

    Add files via upload

---
+++ LUAS PEESEGI.py
@@ -0,0 +1,10 @@
+# nama : nurul auliya
+# nim : D0425324
+# luas persegi
+
+s = int(input("Masukkan sisi pertama: "))
+s2 = int(input("Masukkan sisi kedua: "))
+
+luas = int(s * s2 )
+
+print('luas persegi adalah' , luas)
---
+++ LUAS SGITIGA.py
@@ -0,0 +1,11 @@
+# nama : nurul auliya
+# nim : D0425324
+# luas segitiga
+
+
+alas = int(input("masukkan alas segitiga: "))
+tinggi = int(input("Masukkan tinngi segitiga: "))
+
+luas = int((alas * tinggi) / 2) 
+
+print('luas segitiga adalah: ' , luas)
